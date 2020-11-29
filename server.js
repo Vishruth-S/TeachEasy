05f04f130935619a5286f8ee551a1d1dc100d7ce
Vishruth S <vs.vishruth.s@gmail.com>
2020-11-29 20:05:54 +0530

    initial working server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server)
+const { v4: uuidv4 } = require('uuid');
+
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+})
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+app.use('/peerjs', peerServer)
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        // console.log("Joined room")
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+    })
+})
+server.listen(3030);
