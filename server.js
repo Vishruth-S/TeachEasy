2f700a193153e79694410b70fb61014acdb00b20
Vishruth S <vs.vishruth.s@gmail.com>
2020-12-01 15:58:42 +0530

    added whiteboard, revert screenshare

--- server.js
+++ server.js
@@ -10,11 +10,15 @@ const peerServer = ExpressPeerServer(server, {
 })
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({
+    extended: true
+}))
 
 app.use('/peerjs', peerServer)
 app.get('/', (req, res) => {
-    res.redirect(`/${uuidv4()}`)
+    // res.redirect(`/${uuidv4()}`)
+    res.render('home', { id: uuidv4() })
 })
 
 app.get('/:room', (req, res) => {
@@ -22,14 +26,30 @@ app.get('/:room', (req, res) => {
 })
 
 
+app.post('/create', (req, res) => {
+    const name = req.body.name
+    const id = req.body.code
+    res.redirect(`/${id}`)
+})
+
+app.post('/join', (req, res) => {
+    const name = req.body.name
+    const id = req.body.code
+    res.redirect(`/${id}`)
+})
+
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
         socket.to(roomId).broadcast.emit('user-connected', userId);
 
         socket.on('message', message => {
-            io.to(roomId).emit('createMessage', message)
+            io.to(roomId).emit('createMessage', message, userId)
+        })
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
         })
     })
+    socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
 })
-server.listen(3030);  
+server.listen(process.env.PORT || 3030);  
