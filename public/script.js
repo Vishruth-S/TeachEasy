39e51a5cdb0aac4748eaf986ce185f54eff44b4a
Vishruth S <vs.vishruth.s@gmail.com>
2020-12-01 15:58:19 +0530

    fixed style bugs

--- public/script.js
+++ public/script.js
@@ -1,98 +1,93 @@
-
+///////////////////////////////////////////////////////////
 const socket = io('/')
-
-const videoGrid = document.getElementById('video-grid')
-var peer = new Peer(undefined, {
-    path: "/peerjs",
-    host: "/",
-    port: "3030",
-});
+const videoGrid = document.getElementById('videoGrid')
 const myVideo = document.createElement('video')
 myVideo.muted = true
 
+var peer = new Peer()
 
+const myPeer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '5000',
+})
 
+const peers = {}
 let myVideoStream
-
-
-var getUserMedia =
-    navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia;
-
 navigator.mediaDevices
     .getUserMedia({
         video: true,
         audio: true,
-    }).then(stream => {
+    })
+    .then((stream) => {
         myVideoStream = stream
         addVideoStream(myVideo, stream)
 
-        peer.on('call', call => {
-            call.answer(stream)
-            const video = document.createElement('video')
-            call.on('stream', userVideoStream => {
-                addVideoStream(video, userVideoStream)
-            })
-        })
         socket.on('user-connected', (userId) => {
             connectToNewUser(userId, stream)
+            // alert('Somebody connected', userId)
+        })
+
+        peer.on('call', (call) => {
+            call.answer(stream)
+            const video = document.createElement('video')
+            call.on('stream', (userVideoStream) => {
+                addVideoStream(video, userVideoStream)
+            })
         })
 
         let text = $('input')
 
-
-        $('html').keydown(e => {
-            if (e.which === 13 && text.val().length !== 0) {
+        $('html').keydown(function (e) {
+            if (e.which == 13 && text.val().length !== 0) {
                 socket.emit('message', text.val())
                 text.val('')
             }
         })
 
-        socket.on('createMessage', message => {
-            $('ul').append(`<li class="message"><b>User</b><br/>${message}</li>`)
-            scrollToBottom();
+        socket.on('createMessage', (message, userId) => {
+            $('ul').append(`<li >
+								<span class="messageHeader">
+									<span>
+										From 
+										<span class="messageSender">${userId}</span> 
+										to 
+										<span class="messageReceiver">Everyone:</span>
+									</span>
+
+									${new Date().toLocaleString('en-US', {
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true,
+            })}
+								</span>
+
+								<span class="message">${message}</span>
+							
+							</li>`)
+            scrollToBottom()
         })
     })
+socket.on('user-disconnected', (userId) => {
+    if (peers[userId]) peers[userId].close()
+})
 
-peer.on("call", (call) => {
-    getUserMedia(
-        { video: true, audio: true },
-        (stream) => {
-            call.answer(stream); // Answer the call with an A/V stream.
-            const video = document.createElement("video");
-            call.on("stream", function (remoteStream) {
-                addVideoStream(video, remoteStream);
-            });
-        },
-        (err) => {
-            console.log("Failed to get local stream", err);
-        }
-    );
-});
-
-
-peer.on('open', id => {
+peer.on('open', (id) => {
     socket.emit('join-room', ROOM_ID, id)
 })
 
-
-
 const connectToNewUser = (userId, stream) => {
-    console.log(stream)
     const call = peer.call(userId, stream)
     const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
-        console.log(video)
+    call.on('stream', (userVideoStream) => {
         addVideoStream(video, userVideoStream)
-        console.log(userVideoStream)
     })
     call.on('close', () => {
         video.remove()
     })
-    console.log('new user', userId)
-}
 
+    peers[userId] = call
+}
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream
@@ -100,65 +95,104 @@ const addVideoStream = (video, stream) => {
         video.play()
     })
     videoGrid.append(video)
-    console.log(videoGrid)
 }
 
 const scrollToBottom = () => {
-    let d = $('.main__chat__window')
-    d.scrollTop(d.prop("scrollHeight"))
+    var d = $('.mainChatWindow')
+    d.scrollTop(d.prop('scrollHeight'))
 }
 
 const muteUnmute = () => {
-    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    const enabled = myVideoStream.getAudioTracks()[0].enabled
     if (enabled) {
-        myVideoStream.getAudioTracks()[0].enabled = false;
-        setUnmuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = false
+        setUnmuteButton()
     } else {
-        setMuteButton();
-        myVideoStream.getAudioTracks()[0].enabled = true;
-    }
-}
-
-const playStop = () => {
-    console.log('object')
-    let enabled = myVideoStream.getVideoTracks()[0].enabled;
-    if (enabled) {
-        myVideoStream.getVideoTracks()[0].enabled = false;
-        setPlayVideo()
-    } else {
-        setStopVideo()
-        myVideoStream.getVideoTracks()[0].enabled = true;
+        setMuteButton()
+        myVideoStream.getAudioTracks()[0].enabled = true
     }
 }
 
 const setMuteButton = () => {
     const html = `
-      <i class="fas fa-microphone"></i>
-      <span>Mute</span>
-    `
-    document.querySelector('.main__mute_button').innerHTML = html;
+	  <i class="fas fa-microphone"></i>
+	  <span>Mute</span>
+	`
+    document.querySelector('.mainMuteButton').innerHTML = html
 }
 
 const setUnmuteButton = () => {
     const html = `
-      <i class="unmute fas fa-microphone-slash"></i>
-      <span>Unmute</span>
-    `
-    document.querySelector('.main__mute_button').innerHTML = html;
+	  <i class="unmute fas fa-microphone-slash"></i>
+	  <span>Unmute</span>
+	`
+    document.querySelector('.mainMuteButton').innerHTML = html
+}
+
+const playStop = () => {
+    console.log('object')
+    let enabled = myVideoStream.getVideoTracks()[0].enabled
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false
+        setPlayVideo()
+    } else {
+        setStopVideo()
+        myVideoStream.getVideoTracks()[0].enabled = true
+    }
 }
 
 const setStopVideo = () => {
     const html = `
-      <i class="fas fa-video"></i>
-      <span>Stop Video</span>
-    `
-    document.querySelector('.main__video_button').innerHTML = html;
+	  <i class="fas fa-video"></i>
+	  <span>Stop Video</span>
+	`
+    document.querySelector('.mainVideoButton').innerHTML = html
 }
 
 const setPlayVideo = () => {
     const html = `
-    <i class="stop fas fa-video-slash"></i>
-      <span>Play Video</span>
-    `
-    document.querySelector('.main__video_button').innerHTML = html;
+	<i class="stop fas fa-video-slash"></i>
+	  <span>Play Video</span>
+	`
+    document.querySelector('.mainVideoButton').innerHTML = html
 }
+
+///////////////////////////////////
+// const stopScreenShare = () => {
+//     let videoTrack = myVideoStream.getVideoTracks()[0]
+//     var sender = currentPeer.getSenders().find(s => {
+//         return s.track.kind == videoTrack.kind
+//     })
+//     sender.replaceTrack(videoTrack)
+//     // sender.removeTrack()
+
+// }
+//============screen=============
+
+//     document.getElementById("shareScreen").addEventListener('click', e => {
+//         navigator.mediaDevices.getDisplayMedia({
+//             video: {
+//                 cursor: "always"
+//             },
+//             audio: {
+//                 echoCancellation: true,
+//                 noiseSuppression: true
+//             }
+//         }).then(stream => {
+//             let videoTrack = stream.getVideoTracks()[0]
+//             videoTrack.onended = () => {
+//                 stopScreenShare()
+//             }
+//             let sender = currentPeer.getSenders().find(s => {
+//                 console.log(s.track.kind)
+//                 return s.track.kind == videoTrack.kind
+//             })
+//             console.log("sender", sender)
+//             sender.replaceTrack(videoTrack)
+//         }).catch(err => {
+//             console.log("unable to share screen", err)
+//         })
+//     })
+// })
+
+
